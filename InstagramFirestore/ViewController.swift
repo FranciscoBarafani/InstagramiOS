87b659837a107f869ef2de7a1fdf327a1766717d
BarafaniFrancisco <franbarafani@gmail.com>
2021-07-19 11:42:42 -0300

    Initial Commit

---
+++ InstagramFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestore
+//
+//  Created by Francisco Barafani on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
